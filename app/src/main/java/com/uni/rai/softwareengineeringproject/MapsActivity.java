cb10ff38ec812dcf15bc75990ff09c68efec4620
ItsTheRai <rmndss@hotmail.com>
2015-10-13 20:28:38 +0100

    The application now centers in on the users locaton

--- app/src/main/java/com/uni/rai/softwareengineeringproject/MapsActivity.java
+++ app/src/main/java/com/uni/rai/softwareengineeringproject/MapsActivity.java
@@ -1,17 +1,27 @@
 package com.uni.rai.softwareengineeringproject;
+
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
@@ -22,12 +32,13 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.text.DateFormat;
 import java.util.Date;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener{
+        GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
     private static final long LOCATION_REQUEST_INTERVAL = 1000;//ms
     private static final long FASTEST_LOCATION_INTERVAL = 5000;//ms
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
@@ -41,18 +52,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        //since API 23, need to ask for user permission to use location
+        //so create a check and dialog box
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explenation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0   //could not find the right constant, 0 seems to work for now
+                );//MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+//        reate a new location request for the map locator
+        createLocationRequest();
         //build a google api client
         buildGoogleApiClient();
-        //connect to the server
-        mGoogleApiClient.connect();
-//        create a new location request for the map locator
-        createLocationRequest();
+        //set view as the map activity
+        setContentView(R.layout.activity_maps);
         //init the map
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         //call onMapReady()
         mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart fired ..............");
+        //connect to the server
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop fired ..............");
+        mGoogleApiClient.disconnect();
+        Log.d(TAG, "isConnected ...............: " + mGoogleApiClient.isConnected());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+        Log.d(TAG, "Location update stopped .......................");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+            Log.d(TAG, "Location update resumed .....................");
+        }
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -70,16 +139,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);   //request highest possible accuracy from device
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMyLocationEnabled(true);
     }
+
     @Override
     //updated the location variables once a connection is established with the google server
     public void onConnected(Bundle connectionHint) {
@@ -102,7 +168,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             dialog.show();
         }
 
-        startLocationUpdates(); //start updating the location variables
+        startLocationUpdates(); //start listening for location changes
         if (mCurrentLocation != null) {
             // only add marker on the map if the current location is found
             ((MapFragment) getFragmentManager()
@@ -111,7 +177,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         } else {
             // if the current location could not be found, display a dialog that redirect the user to the location setting screen
             LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-            if(!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+            if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
                     !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("Location Services Not Active");
@@ -131,7 +197,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     //request location udates from google
+//    @TargetApi(Build.VERSION_CODES.M)
     protected void startLocationUpdates() {
+        // Register the listener with the Location Manager to receive location updates
+//        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for Activity#requestPermissions for more details.
+//            return;
+//        }
+//        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if (mLastLocation == null) {
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
