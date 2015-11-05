762d593d52c61a3d6731710b36214f20673feaca
ItsTheRai <rmndss@hotmail.com>
2015-11-05 21:53:36 +0000

    entity tags removed

--- backend/src/main/java/com/example/rai/myapplication/backend/model/SalesDataShort.java
+++ backend/src/main/java/com/example/rai/myapplication/backend/model/SalesDataShort.java
@@ -1,24 +1,20 @@
 package com.example.rai.myapplication.backend.model;
 
 import com.google.appengine.api.datastore.GeoPt;
-import com.googlecode.objectify.annotation.Entity;
-import com.googlecode.objectify.annotation.Id;
 
 /**
  * Created by rai on 03/11/15.
  */
-@Entity
-public class SalesLocationData {
-    @Id
-            private Long id;
+public class SalesDataShort {
+    private Long id;
     String transId;
     int priceInPouds;
     GeoPt location;
 
-    public SalesLocationData(){
+    public SalesDataShort(){
     }
 
-    public SalesLocationData(String transId, int priceInPouds, GeoPt location) {
+    public SalesDataShort(String transId, int priceInPouds, GeoPt location) {
         this.transId = transId;
         this.priceInPouds = priceInPouds;
         this.location = location;
