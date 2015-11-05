489e68a58ed5d9268732083f24cd47946f2ecf5a
ItsTheRai <rmndss@hotmail.com>
2015-11-05 22:12:47 +0000

    cleanup

--- app/src/main/java/com/uni/rai/softwareengineeringproject/tasks/UpdateLocationAsyncTask.java
+++ app/src/main/java/com/uni/rai/softwareengineeringproject/tasks/UpdateLocationAsyncTask.java
@@ -5,7 +5,8 @@ import android.os.AsyncTask;
 import android.widget.Toast;
 
 import com.example.rai.myapplication.backend.salesInformationApi.model.GeoPt;
-import com.example.rai.myapplication.backend.salesInformationApi.model.SalesInformation;
+import com.example.rai.myapplication.backend.salesInformationApi.model.SalesData;
+//import com.example.rai.myapplication.backend.salesInformationApi.model.SalesInformation;
 import com.example.rai.myapplication.backend.userLocationApi.UserLocationApi;
 //import com.example.rai.myapplication.backend.userLocationApi.model.UserLocation;
 import com.example.rai.myapplication.backend.salesInformationApi.SalesInformationApi;
@@ -85,60 +86,32 @@ public class UpdateLocationAsyncTask extends AsyncTask<Void, Void, Void> {
 //        );
             myApiService2 = builder.build();
         }
-
-
-     try {
-            SalesInformation a = new SalesInformation();
-            a.setCounty("couty");
-            a.setDate("Date");
-            a.setDistrict("District");
-            a.setDuration("Duration");
-            a.setLocality("Locality");
-            GeoPt pt = new GeoPt();
-            pt.setLatitude((float) 50.8680860);
-            pt.setLongitude((float) -0.0904250);
-            a.setLocation(pt);
-            a.setOldOrNew("OldOrNew");
-            a.setPaon("PAON");
-            a.setPostcode("POSTCODE");
-            a.setPpdcategory("PPDCat");
-            a.setPrice("Price");
-            a.setPropertyType("PriopertyType");
-            a.setSaon("SAON");
-            a.setStreet("Street");
-            a.setTown("Town");
-            a.setUniqueRef("UNIQUREF");
-            myApiService2.insert(a);//.updateUserLocation(params[0]).execute();
-
-         SalesInformation b = new SalesInformation();
-         b.setCounty("couty");
-         b.setDate("Date");
-         b.setDistrict("District");
-         b.setDuration("Duration");
-         b.setLocality("Locality");
-         GeoPt p = new GeoPt();
-         p.setLatitude((float)50.8680830);
-         p.setLongitude((float) -0.0904220);
-         b.setLocation(p);
-         b.setOldOrNew("OldOrNew");
-         b.setPaon("PAON");
-         b.setPostcode("POSTCODE");
-         b.setPpdcategory("PPDCat");
-         b.setPrice("Price");
-         b.setPropertyType("PriopertyType");
-         b.setSaon("SAON");
-         b.setStreet("Street");
-         b.setTown("Town");
-         b.setUniqueRef("UNIQUREF");
-         myApiService2.insert(b);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//            SalesData a = new SalesData();
+//            a.setCounty("couty");
+////            a.setDate("Date");
+//            a.setDistrict("District");
+//            a.setDuration("Duration");
+//            a.setLocality("Locality");
+//            GeoPt pt = new GeoPt();
+//            a.setLatitude((float) 50.8680860);
+//            a.setLongitude((float) -0.0904250);
+////            a.setLocation(pt);
+//            a.setOldOrNew("OldOrNew");
+//            a.setPaon("PAON");
+//            a.setPostcode("POSTCODE");
+//            a.setPpdcategory("PPDCat");
+//            a.setPrice(000);
+//            a.setPropertyType("PriopertyType");
+//            a.setSaon("SAON");
+//            a.setStreet("Street");
+//            a.setTown("Town");
+//            a.setUniqueRef("UNIQUREF");
+//            myApiService2.insert(a).execute();
         return null;
     }
 
 //    @Override
-//    protected void onPostExecute(List<UserLocation> result) {
+//    protected void onPostExecute(Void... result) {
 //        for (Quote q : result) {
 //            Toast.makeText(context, q.getWho() + " : " + q.getWhat(), Toast.LENGTH_LONG).show();
 //        }
