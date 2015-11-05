e5d07af940a3c07c790d14cd1c817763d7fdb628
ItsTheRai <rmndss@hotmail.com>
2015-11-05 22:05:40 +0000

    check added before commiting entity

--- backend/src/main/java/com/example/rai/myapplication/backend/SalesInformationEndpoint.java
+++ backend/src/main/java/com/example/rai/myapplication/backend/SalesInformationEndpoint.java
@@ -1,11 +1,7 @@
 package com.example.rai.myapplication.backend;
 
-import com.example.rai.myapplication.backend.model.SalesInformation;
-import com.example.rai.myapplication.backend.model.SalesLocationData;
-//import com.example.rai.myapplication.
-//import com.example.rai.myapplication.backend.salesInformationApi.model.SalesLocationDataCollection;
-import com.example.rai.myapplication.backend.model.SalesInformation;
-import com.example.rai.myapplication.backend.model.SalesLocationData;
+import com.example.rai.myapplication.backend.model.SalesData;
+import com.example.rai.myapplication.backend.model.SalesDataShort;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -24,13 +20,7 @@ import com.googlecode.objectify.cmd.Query;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
-/**
- * WARNING: This generated code is intended as a sample or starting point for using a
- * Google Cloud Endpoints RESTful API with an Objectify entity. It provides no data access
- * restrictions and no data validation.
- * <p>
- * DO NOT deploy this code unchanged as part of a real application to real users.
- */
+
 @Api(
         name = "salesInformationApi",
         version = "v1",
@@ -48,87 +38,77 @@ public class SalesInformationEndpoint {
     private static final int DEFAULT_LIST_LIMIT = 20;
 
     /**
-     * Returns the {@link SalesInformation} with the corresponding ID.
+     * Returns the {@link SalesData} with the corresponding ID.
      *
      * @param id the ID of the entity to be retrieved
      * @return the entity with the corresponding ID
-     * @throws NotFoundException if there is no {@code SalesInformation} with the provided ID.
+     * @throws NotFoundException if there is no {@code SalesData} with the provided ID.
      */
     @ApiMethod(
             name = "get",
             path = "salesInformation/{id}",
             httpMethod = ApiMethod.HttpMethod.GET)
-    public SalesInformation get(@Named("id") Long id) throws NotFoundException {
-        logger.info("Getting SalesInformation with ID: " + id);
-//        SalesInformation salesInformation = ofy().load().type(SalesInformation.class).id(id).now();
-//        if (salesInformation == null) {
-//            throw new NotFoundException("Could not find SalesInformation with ID: " + id);
-//        }
-//        return salesInformation;
-        return null;
+    public SalesData get(@Named("id") Long id) throws NotFoundException {
+        logger.info("Getting SalesData with ID: " + id);
+        SalesData salesInformation = ofy().load().type(SalesData.class).id(id).now();
+        if (salesInformation == null) {
+            throw new NotFoundException("Could not find SalesData with ID: " + id);
+        }
+        return salesInformation;
     }
 
     @ApiMethod(
             name ="getPointsInRange",
             httpMethod = ApiMethod.HttpMethod.GET)
-    public List<SalesLocationData> getPointsInRange(@Named("rangeInMiles") float rangeInMiles,
-                                                        @Named("latitude")float latitude,
-                                                        @Named("longitude")float longitude,
-                                                        @Named("maxLength") int maxLength){
-
-
-
-        List<SalesLocationData> places = NearPlacesFinder
-                .getPlaces(new GeoPt(latitude,longitude), rangeInMiles, maxLength);   //returns point in range of the user in specific range
+    public List<SalesDataShort> getPointsInRange(@Named("rangeInMiles") float rangeInMiles, @Named("latitude")float latitude,
+                                                        @Named("longitude")float longitude, @Named("maxLength") int maxLength){
+        //returns point in range of the user using helper class
+        List<SalesDataShort> places = NearPlacesFinder.getPlaces(new GeoPt(latitude, longitude), rangeInMiles, maxLength);
         return places;
-//        return null;//temp value
     }
 
     /**
-     * Inserts a new {@code SalesInformation}.
+     * Inserts a new {@code SalesData}.
      */
     @ApiMethod(
             name = "insert",
-            path = "salesInformation",
+            path = "salesData",
             httpMethod = ApiMethod.HttpMethod.POST)
-    public SalesInformation insert(SalesInformation salesInformation) {
+    public SalesData insert(SalesData salesData) throws NotFoundException {
         // Typically in a RESTful API a POST does not have a known ID (assuming the ID is used in the resource path).
-        // You should validate that salesInformation.id has not been set. If the ID type is not supported by the
+        // You should validate that salesData.id has not been set. If the ID type is not supported by the
         // Objectify ID generator, e.g. long or String, then you should generate the unique ID yourself prior to saving.
         //
         // If your client provides the ID then you should probably use PUT instead.
-//        ofy().save().entity(salesInformation).now();
-//        logger.info("Created SalesInformation with ID: " + salesInformation.getId());
-//
-//        return ofy().load().entity(salesInformation).now();
-        return null;
+        checkExists(salesData.getId());
+        ofy().save().entity(salesData).now();
+        logger.info("Created SalesData with ID: " + salesData.getId());
+        return ofy().load().entity(salesData).now();
     }
-
-
 
     @ApiMethod(
             name = "list",
             path = "salesInformation",
             httpMethod = ApiMethod.HttpMethod.GET)
-    public CollectionResponse<SalesInformation> list(@Nullable @Named("cursor") String cursor, @Nullable @Named("limit") Integer limit) {
+    public CollectionResponse<SalesData> list(@Nullable @Named("cursor") String cursor, @Nullable @Named("limit") Integer limit) {
         limit = limit == null ? DEFAULT_LIST_LIMIT : limit;
-        Query<SalesInformation> query = ofy().load().type(SalesInformation.class).limit(limit);
+        Query<SalesData> query = ofy().load().type(SalesData.class).limit(limit);
         if (cursor != null) {
             query = query.startAt(Cursor.fromWebSafeString(cursor));
         }
-        QueryResultIterator<SalesInformation> queryIterator = query.iterator();
-        List<SalesInformation> salesInformationList = new ArrayList<SalesInformation>(limit);
+        QueryResultIterator<SalesData> queryIterator = query.iterator();
+        List<SalesData> salesDataList = new ArrayList<SalesData>(limit);
         while (queryIterator.hasNext()) {
-            salesInformationList.add(queryIterator.next());
+            salesDataList.add(queryIterator.next());
         }
-        return CollectionResponse.<SalesInformation>builder().setItems(salesInformationList).setNextPageToken(queryIterator.getCursor().toWebSafeString()).build();
+        return CollectionResponse.<SalesData>builder().setItems(salesDataList).setNextPageToken(queryIterator.getCursor().toWebSafeString()).build();
     }
 
     private void checkExists(Long id) throws NotFoundException {
         try {
-            ofy().load().type(SalesInformation.class).id(id).safe();
+            ofy().load().type(SalesData.class).id(id).safe();
         } catch (com.googlecode.objectify.NotFoundException e) {
-            throw new NotFoundException("Could not find SalesInformation with ID: " + id);
+            throw new NotFoundException("Could not find SalesData with ID: " + id);
         }
     }
 }
