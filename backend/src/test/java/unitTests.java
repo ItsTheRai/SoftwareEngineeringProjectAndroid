3a27b8f29af38678fc178c4b23b087aceacdad15
lukedove <lukedove@hotmail.com>
2015-11-11 10:31:53 +0000

    Added first backend test cases.

--- backend/src/test/java/unitTests.java
+++ backend/src/test/java/unitTests.java
@@ -2,15 +2,30 @@
  * Created by Luke on 08/11/2015.
  */
 
+import com.example.rai.myapplication.backend.IndexingServlet;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class unitTests {
 
-    @Test
-    public void test()
-    {
+    IndexingServlet indexingServlet;
 
+    //Checks to see if IndexServlet does not return null.
+    @Test
+    public void checkIndexServlet()
+    {
+        indexingServlet = new IndexingServlet();
+        assertNotNull(indexingServlet);
+
+    }
+
+    //Checks to see if IndexServlet returns null.  Should fail.
+    @Test
+    public void checkNull()
+    {
+        indexingServlet = new IndexingServlet();
+        assertNull(indexingServlet);
     }
 }
