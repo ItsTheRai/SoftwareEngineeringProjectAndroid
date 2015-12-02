6aeaad53a0e88c8a3b6523647835e598702dddc8
ItsTheRai <rmndss@hotmail.com>
2015-12-02 19:32:34 +0000

    OnDataSent is a helper class to call the background task from the main class to update the heatmap

---
+++ app/src/main/java/com/uni/rai/softwareengineeringproject/tasks/OnDataSendToActivity.java
@@ -0,0 +1,10 @@
+package com.uni.rai.softwareengineeringproject.tasks;
+
+import java.util.List;
+
+/**
+ * Created by rai on 01/12/15.
+ */
+public interface OnDataSendToActivity {
+    public void sendData(List<List<Double>> list);
+}
